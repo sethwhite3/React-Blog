cfbdc07271b108abc9c1ef24b51752a67fb913c3
Seth White <sethewhite@me.com>
2020-01-24 13:24:16 -0500

    First commit

---
+++ src/build/precache-manifest.f34c75bcd3540f70e29ceb99ecb26c5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd1ae024365970ae68d716b583bd9ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70b734825aa6d446006d",
+    "url": "/static/css/main.7abc6471.chunk.css"
+  },
+  {
+    "revision": "54c8cbfcfe6f894408c7",
+    "url": "/static/js/2.73a1cec7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.73a1cec7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70b734825aa6d446006d",
+    "url": "/static/js/main.3c2dd63d.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
